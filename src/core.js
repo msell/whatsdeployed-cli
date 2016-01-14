d03cbe61c75087ee39801c3675c8eb1c94e6266a
Matt Sell <mwsell@gmail.com>
2016-01-13 21:59:47 -0600

    lookup server

--- src/core.js
+++ src/core.js
@@ -1,13 +1,49 @@
 import request from 'superagent';
+import {find} from 'lodash';
+import {red, green, underline} from 'chalk';
+
+const url = "http://whatsdeployed.herokuapp.com";
 
 export function whatsOn(server) {  
-  const promise = new Promise(function(resolve, reject){
-    resolve([]);
-  });
-  
-  return promise;
+  return new Promise(function (resolve, reject) {
+		  getServers().then(function(servers){
+        const match = find(servers, {'name':server});
+        if(!match){
+          console.log(red(underline(server) + " was not found"));
+          reject("no match");
+        }
+        // if exists make call to get applications
+      })
+	});
 };
 
 export function diff (server1, server2) {
 
 };
+
+
+function getServers() {
+	return new Promise(function (resolve, reject) {
+		request.get(`${url}/servers.json`)
+			.end(function (err, res) {
+				if (err) reject(err.message);
+
+				if (!err && res.statusCode == 200) {
+					resolve(res.body);
+				}
+			})
+	})
+}
+
+function getApplications(serverId) {
+	return new Promise(function (resolve, reject) {
+		request.get(`${url}/servers/${serverId}/deployed_apps.json`)
+			.end(function (err, res) {
+				if (err) reject(err.message);
+
+				if (!err && res.statusCode == 200) {
+					resolve(res.body);
+				}
+			})
+	})
+}
--- src/whatsdeployed-cli.js
+++ src/whatsdeployed-cli.js
@@ -1,45 +1,20 @@
 #!/usr/bin/env node
 
-import * as git from 'git-rev';
 import {red, green, underline} from 'chalk';
 import {argv} from 'yargs';
-import {readFile, writeFile} from 'fs';
-import * as path from 'path';
-import * as process from 'process';
 import prompt from 'cli-prompt';
-import {plans} from './bamboo.js';
+import * as process from 'process';
+import {whatsOn, diff} from './core.js';
 
-let branch = 'folder not under Git source control';
-let settings = null;
-let projectName = path.basename(process.cwd());
-
-var getBranch = new Promise(function (resolve, reject) {
-  git.branch(function (str) {
-    branch = str;
-    resolve(str);
-  })
-});
-
-
-if (argv._.find(x=> x === 'diff')) {
-  getBranch.then(
-    function () {
-
-      console.log('project name ' + projectName);
-
-      console.log(`you must be tryin to kick a build on ${green.underline(branch)} but that functionality is not implemented yet.`);
+if (process.argv.length < 3) {
+  console.log(red("off to see the wizard..."));
+} else {
+  const server = process.argv[2].toLowerCase();
+  whatsOn(server).then(function (apps) {
+    for (const app of apps) {
+      console.log(green(app.name));
     }
-
-    )
-    .catch(function (reason) {
-      console.log(`damnit jim ${reason}`);
-    })
+  });
 
 }
 
-if(argv._.find(x=> x === 'plans')){
-  plans().then(function(data){
-    console.log(data);
-  })
-
-}
